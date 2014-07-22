4d45cc4557d82509648e03a8d5b349554b1eb429
zs1621 <zs1213yh@gmail.com>
2014-07-22 17:24:30 +0800

    fix

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
       res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end('Hello World\n');
-}).listen(1333, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1333/');
+}).listen(1333, 'localhost');
+console.log('Server running at http://local:1333/');
